0381072c8bf3542ba607d0a0167d5308937bdb85
pepinovimr <pepino.vimr@seznam.cz>
2022-10-03 22:29:27 +0200

    Changed layout of form

--- Phonebook/PresentationLayer/PhonebookForm.Designer.cs
+++ Phonebook/PresentationLayer/PhonebookForm.Designer.cs
@@ -46,6 +46,10 @@
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.addContactBtn = new System.Windows.Forms.Button();
+            this.deleteContactBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
             emailLabel = new System.Windows.Forms.Label();
             firstNameLabel = new System.Windows.Forms.Label();
             genderLabel = new System.Windows.Forms.Label();
@@ -53,6 +57,7 @@
             phoneNumberLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.contactDataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.contactBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // contactDataGridView
@@ -71,7 +76,7 @@
             this.dataGridViewTextBoxColumn4,
             this.dataGridViewTextBoxColumn5});
             this.contactDataGridView.DataSource = this.contactBindingSource;
-            this.contactDataGridView.Location = new System.Drawing.Point(12, 194);
+            this.contactDataGridView.Location = new System.Drawing.Point(12, 189);
             this.contactDataGridView.Name = "contactDataGridView";
             this.contactDataGridView.ReadOnly = true;
             this.contactDataGridView.Size = new System.Drawing.Size(549, 275);
@@ -80,7 +85,7 @@
             // emailLabel
             // 
             emailLabel.AutoSize = true;
-            emailLabel.Location = new System.Drawing.Point(311, 116);
+            emailLabel.Location = new System.Drawing.Point(167, 104);
             emailLabel.Name = "emailLabel";
             emailLabel.Size = new System.Drawing.Size(35, 13);
             emailLabel.TabIndex = 1;
@@ -89,24 +94,24 @@
             // emailTextBox
             // 
             this.emailTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.contactBindingSource, "Email", true));
-            this.emailTextBox.Location = new System.Drawing.Point(398, 113);
+            this.emailTextBox.Location = new System.Drawing.Point(254, 101);
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailTextBox.Size = new System.Drawing.Size(307, 20);
             this.emailTextBox.TabIndex = 2;
             // 
             // firstNameLabel
             // 
             firstNameLabel.AutoSize = true;
-            firstNameLabel.Location = new System.Drawing.Point(311, 36);
+            firstNameLabel.Location = new System.Drawing.Point(167, 28);
             firstNameLabel.Name = "firstNameLabel";
-            firstNameLabel.Size = new System.Drawing.Size(60, 13);
+            firstNameLabel.Size = new System.Drawing.Size(41, 13);
             firstNameLabel.TabIndex = 3;
-            firstNameLabel.Text = "First Name:";
+            firstNameLabel.Text = "Jméno:";
             // 
             // firstNameTextBox
             // 
             this.firstNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.contactBindingSource, "FirstName", true));
-            this.firstNameTextBox.Location = new System.Drawing.Point(398, 33);
+            this.firstNameTextBox.Location = new System.Drawing.Point(254, 25);
             this.firstNameTextBox.Name = "firstNameTextBox";
             this.firstNameTextBox.Size = new System.Drawing.Size(100, 20);
             this.firstNameTextBox.TabIndex = 4;
@@ -114,33 +119,33 @@
             // genderLabel
             // 
             genderLabel.AutoSize = true;
-            genderLabel.Location = new System.Drawing.Point(311, 88);
+            genderLabel.Location = new System.Drawing.Point(167, 64);
             genderLabel.Name = "genderLabel";
-            genderLabel.Size = new System.Drawing.Size(45, 13);
+            genderLabel.Size = new System.Drawing.Size(47, 13);
             genderLabel.TabIndex = 5;
-            genderLabel.Text = "Gender:";
+            genderLabel.Text = "Pohlaví:";
             // 
             // genderTextBox
             // 
             this.genderTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.contactBindingSource, "Gender", true));
-            this.genderTextBox.Location = new System.Drawing.Point(398, 85);
+            this.genderTextBox.Location = new System.Drawing.Point(254, 61);
             this.genderTextBox.Name = "genderTextBox";
-            this.genderTextBox.Size = new System.Drawing.Size(100, 20);
+            this.genderTextBox.Size = new System.Drawing.Size(66, 20);
             this.genderTextBox.TabIndex = 6;
             // 
             // lastNameLabel
             // 
             lastNameLabel.AutoSize = true;
-            lastNameLabel.Location = new System.Drawing.Point(518, 36);
+            lastNameLabel.Location = new System.Drawing.Point(384, 28);
             lastNameLabel.Name = "lastNameLabel";
-            lastNameLabel.Size = new System.Drawing.Size(61, 13);
+            lastNameLabel.Size = new System.Drawing.Size(51, 13);
             lastNameLabel.TabIndex = 7;
-            lastNameLabel.Text = "Last Name:";
+            lastNameLabel.Text = "Příjmení:";
             // 
             // lastNameTextBox
             // 
             this.lastNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.contactBindingSource, "LastName", true));
-            this.lastNameTextBox.Location = new System.Drawing.Point(605, 33);
+            this.lastNameTextBox.Location = new System.Drawing.Point(461, 25);
             this.lastNameTextBox.Name = "lastNameTextBox";
             this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
             this.lastNameTextBox.TabIndex = 8;
@@ -148,18 +153,18 @@
             // phoneNumberLabel
             // 
             phoneNumberLabel.AutoSize = true;
-            phoneNumberLabel.Location = new System.Drawing.Point(311, 142);
+            phoneNumberLabel.Location = new System.Drawing.Point(334, 64);
             phoneNumberLabel.Name = "phoneNumberLabel";
-            phoneNumberLabel.Size = new System.Drawing.Size(81, 13);
+            phoneNumberLabel.Size = new System.Drawing.Size(82, 13);
             phoneNumberLabel.TabIndex = 9;
-            phoneNumberLabel.Text = "Phone Number:";
+            phoneNumberLabel.Text = "Telefonní číslo:";
             // 
             // phoneNumberTextBox
             // 
             this.phoneNumberTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.contactBindingSource, "PhoneNumber", true));
-            this.phoneNumberTextBox.Location = new System.Drawing.Point(398, 139);
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(421, 61);
             this.phoneNumberTextBox.Name = "phoneNumberTextBox";
-            this.phoneNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(140, 20);
             this.phoneNumberTextBox.TabIndex = 10;
             // 
             // contactBindingSource
@@ -204,11 +209,50 @@
             this.dataGridViewTextBoxColumn5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridViewTextBoxColumn5.Width = 105;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 153);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // addContactBtn
+            // 
+            this.addContactBtn.Location = new System.Drawing.Point(170, 138);
+            this.addContactBtn.Name = "addContactBtn";
+            this.addContactBtn.Size = new System.Drawing.Size(90, 27);
+            this.addContactBtn.TabIndex = 12;
+            this.addContactBtn.Text = "Přidat kontakt";
+            this.addContactBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteContactBtn
+            // 
+            this.deleteContactBtn.Location = new System.Drawing.Point(305, 138);
+            this.deleteContactBtn.Name = "deleteContactBtn";
+            this.deleteContactBtn.Size = new System.Drawing.Size(130, 27);
+            this.deleteContactBtn.TabIndex = 13;
+            this.deleteContactBtn.Text = "Smazat vybraný kontakt";
+            this.deleteContactBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(471, 138);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(90, 27);
+            this.saveBtn.TabIndex = 14;
+            this.saveBtn.Text = "Uložit";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
             // PhonebookForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 581);
+            this.ClientSize = new System.Drawing.Size(572, 487);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.deleteContactBtn);
+            this.Controls.Add(this.addContactBtn);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(emailLabel);
             this.Controls.Add(this.emailTextBox);
             this.Controls.Add(firstNameLabel);
@@ -224,6 +268,7 @@
             this.Text = "PhonebookForm";
             ((System.ComponentModel.ISupportInitialize)(this.contactDataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.contactBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,5 +288,9 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button addContactBtn;
+        private System.Windows.Forms.Button deleteContactBtn;
+        private System.Windows.Forms.Button saveBtn;
     }
 }
